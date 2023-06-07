e3f42ee34588d72ec376376192db31b447eeec74
softlolx <softlolx@gmail.com>
2023-06-07 23:48:01 +0300

    proxy routes

--- controllers/proxies.ts
+++ controllers/proxies.ts
@@ -4,7 +4,9 @@ import axios from "axios";
 export const registerOrder = async (req: Request, res: Response): Promise<void> => {
     try {
         console.log(req.query);
-        const response = await axios.post('https://payment.alfabank.ru/payment/rest/register.do', req.query);
+        const response = await axios.post('https://payment.alfabank.ru/payment/rest/register.do', {}, {
+         params: req.query
+        });
         console.log(response.data);
         res.status(response.status).send(response.data);
     } catch (error: any) {
