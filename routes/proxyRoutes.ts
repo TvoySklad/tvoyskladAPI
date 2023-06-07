ee12c3bd547f9e7aaa0e0760e57012bea05cef51
softlolx <softlolx@gmail.com>
2023-06-07 23:14:04 +0300

    proxy routes

--- routes/proxyRoutes.ts
+++ routes/proxyRoutes.ts
@@ -1,9 +1,13 @@
 import {checkOrderStatus, registerOrder} from "../controllers/proxies";
-
+const cors = require('cors');
 const proxyRouter = require('express').Router();
 
-router.post('/register', registerOrder);
+const proxyCorsOptions = {
+  origin: ['http://pay.tvoysklad.com', 'https://pay.tvoysklad.com', 'http://localhost:3000', 'https://localhost:3000']
+};
 
-router.post('/check', checkOrderStatus);
+router.post('/register', cors(proxyCorsOptions), registerOrder);
+
+router.post('/check', cors(proxyCorsOptions),  checkOrderStatus);
 
 module.exports = proxyRouter;
