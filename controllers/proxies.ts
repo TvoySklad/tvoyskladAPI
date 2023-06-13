3ad9889ff318d917f063ca2f246a83f31c55e310
softlolx <softlolx@gmail.com>
2023-06-13 23:14:54 +0300

    cors added

--- controllers/proxies.ts
+++ controllers/proxies.ts
@@ -1,24 +1,34 @@
 import { Request, Response } from 'express';
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export const registerOrder = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const response = await axios.post('https://payment.alfabank.ru/payment/rest/register.do', {}, {
-         params: req.query
-        });
-        res.status(response.status).send(response.data);
-    } catch (error: any) {
-        res.status(error.response.status).send(error.response.data);
-    }
+  try {
+    const axiosConfig: AxiosRequestConfig = {
+      params: req.query,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+
+    const response = await axios.post('https://payment.alfabank.ru/payment/rest/register.do', {}, axiosConfig);
+    res.status(response.status).send(response.data);
+  } catch (error: any) {
+    res.status(error.response.status).send(error.response.data);
+  }
 };
 
 export const checkOrderStatus = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const response = await axios.post('https://payment.alfabank.ru/payment/rest/getOrderStatus.do', {}, {
-            params: req.query
-        })
-        res.status(response.status).send(response.data);
-    } catch (error: any) {
-        res.status(error.response.status).send(error.response.data);
-    }
+  try {
+    const axiosConfig: AxiosRequestConfig = {
+      params: req.query,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+
+    const response = await axios.post('https://payment.alfabank.ru/payment/rest/getOrderStatus.do', {}, axiosConfig);
+    res.status(response.status).send(response.data);
+  } catch (error: any) {
+    res.status(error.response.status).send(error.response.data);
+  }
 };
