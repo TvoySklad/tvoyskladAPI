11703949735cce81689cd3911a9e4417e433ed9c
softlolx <softlolx@gmail.com>
2023-06-14 18:10:15 +0300

    del comments

--- app.ts
+++ app.ts
@@ -28,28 +28,6 @@ app.use(express.json());
 app.use('/coupons', couponRoutes);
 app.use('/proxy', proxyRoutes);
 
-// app.post('/proxy/register', async (req: Request, res: Response) => {
-//     try {
-//         console.log('Request to /proxy/register:', req.body);
-//         const response = await axios.post('https://payment.alfabank.ru/payment/rest/register.do', req.body);
-//         console.log('Response from Alfabank API:', response.data);
-//         res.status(response.status).send(response.data);
-//     } catch (error: any) {
-//         console.error('Error from Alfabank API:', error.response.data);
-//         res.status(error.response.status).send(error.response.data);
-//     }
-// });
-//
-// app.post('/proxy/check', async (req: Request, res: Response) => {
-//     try {
-//         const response = await axios.post('https://payment.alfabank.ru/payment/rest/getOrderStatus.do', req.body);
-//
-//         res.status(response.status).send(response.data);
-//     } catch (error: any) {
-//         res.status(error.response.status).send(error.response.data);
-//     }
-// });
-
 // Start the server
 const port = 3000;
 app.listen(port, () => {
