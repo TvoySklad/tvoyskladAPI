bde7e15fafd889ec4bce043d21cd3fb71e7fd501
softlolx <softlolx@gmail.com>
2023-08-17 00:13:53 +0300

    link based on request

--- controllers/proxies.ts
+++ controllers/proxies.ts
@@ -2,8 +2,6 @@ import { Request, Response } from 'express';
 import axios, { AxiosRequestConfig } from "axios";
 
 export const registerOrder = async (req: Request, res: Response): Promise<void> => {
-  console.log("register");
-  console.log(req.query);
   try {
     const axiosConfig: AxiosRequestConfig = {
       params: req.query,
@@ -13,7 +11,6 @@ export const registerOrder = async (req: Request, res: Response): Promise<void> 
     };
 
     const response = await axios.post(`${req.query.requestLink}/register.do`, {}, axiosConfig);
-    console.log(response);
     res.status(response.status).send(response.data);
   } catch (error: any) {
     res.status(error.response.status).send(error.response.data);
@@ -21,8 +18,6 @@ export const registerOrder = async (req: Request, res: Response): Promise<void> 
 };
 
 export const checkOrderStatus = async (req: Request, res: Response): Promise<void> => {
-  console.log("check");
-  console.log(req.query);
   try {
     const axiosConfig: AxiosRequestConfig = {
       params: req.query,
@@ -32,7 +27,6 @@ export const checkOrderStatus = async (req: Request, res: Response): Promise<voi
     };
 
     const response = await axios.post(`${req.query.link}/getOrderStatus.do`, {}, axiosConfig);
-    console.log(response);
     res.status(response.status).send(response.data);
   } catch (error: any) {
     res.status(error.response.status).send(error.response.data);
